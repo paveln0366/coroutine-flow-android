0b34a4c69bac7481c89b4caf452b1d7e66baa0f1
Pavel Potapov <potapov@goozix.com>
2025-12-25 22:44:00 +0300

    Add lesson 121

---
+++ app/src/main/java/com/example/coroutineflow/lessons/lesson8/Lesson8.kt
@@ -0,0 +1,35 @@
+package com.example.coroutineflow.lessons.lesson8
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+suspend fun main() {
+    val flow = getFlow()
+
+    val job1 = coroutineScope.launch {
+        flow.collect {
+            println(it)
+        }
+    }
+    val job2 = coroutineScope.launch {
+        flow.collect {
+            println(it)
+        }
+    }
+    job1.join()
+    job2.join()
+}
+
+fun getFlow(): Flow<Int> = flow {
+    repeat(100) {
+        println("Emitted: $it")
+        emit(it)
+        delay(1000)
+    }
+}

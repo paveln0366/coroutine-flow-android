53307b87110756470c8daad0345f2e361de35bea
Pavel Potapov <potapov@goozix.com>
2025-12-25 21:26:14 +0300

    Add lesson 115

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
 }
